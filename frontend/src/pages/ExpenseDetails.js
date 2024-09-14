fba1ac5a148cd24bb3db177d066e0e16cf7967dc
Manav Gupta <gmanav405@gmail.com>
2024-09-14 12:05:37 +0530

    css done

--- frontend/src/pages/ExpenseDetails.js
+++ frontend/src/pages/ExpenseDetails.js
@@ -1,16 +1,16 @@
 import React from 'react'
-
+import './ExpenseDetails.css';
 function ExpenseDetails({ incomeAmt, expenseAmt }) {
     return (
         <div>
-            <div>
+            <div className="Balance-amount">
                 Your Balance is ₹ {incomeAmt - expenseAmt}
             </div>
             {/* Show Income & Expense amount */}
             <div className="amounts-container">
-                Income
+                Income: 
                 <span className="income-amount">₹{incomeAmt}</span>
-                Expense
+                Expense:
                 <span className="expense-amount">₹{expenseAmt}</span>
             </div>
         </div>
--- frontend/src/pages/ExpenseForm.js
+++ frontend/src/pages/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { handleError } from '../utils';
-
+import './ExpenseForm.css';
 function ExpenseForm({ addTransaction }) {
     const [expenseInfo, setExpenseInfo] = useState({
         amount: '',
